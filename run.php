c248a558d8f1113f8ceb3e9c7a7fd4a71deebb30
Yhya syrian <77106078+yhyasyrian@users.noreply.github.com>
2021-05-05 16:45:26 +0300

    Add files via upload

---
+++ run.php
@@ -0,0 +1,18 @@
+<?php
+system("clear");
+echo "Welcome To Tool C++\n";
+while(true){
+  $code = readline("Enter You Code:");
+  if(strtolower($code) != "run"){
+    file_put_contents("test.cpp",$code."\n",FILE_APPEND);
+  }else{
+    $run = shell_exec("c++ test.cpp");
+    if(preg_match('#error:#',$run)){
+      echo $run;
+    }else{
+      echo '\033[0;32m'.shell_exec('./a.out').'\033[0m';
+    }
+    unlink("test.cpp");
+    break;
+  }
+}
